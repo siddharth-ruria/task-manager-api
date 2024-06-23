2101d360caff7ee4701db7d6ce6605d2e0dc6673
Siddharth Ruria <siddharthruria10@gmail.com>
2024-06-23 23:05:37 +0530

    work-1

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+
+const tasks = require("./routes/tasks.js");
+const connectDB = require("./db/connect.js");
+
+require('dotenv').config()
+
+app.get("/", (req, res) => {
+  res.send("Task Manager App");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+const PORT = 5050;
+
+const start= async() => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(PORT, () => {
+      console.log(`app listening on server ${PORT}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+start();
+
